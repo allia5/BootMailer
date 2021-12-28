d9b88a7827b060fa7498685ef84d0810ea668d97
ramzi allia <53237827+allia5@users.noreply.github.com>
2021-12-28 13:41:22 +0100

    v1.2

---
+++ resources/views/partie2/formuller.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+           <div class="input-group d-flex">
+               <input type="text" type="email" placeholder="exemple@gmail.com" class="form-control">
+              <button class="btn btn-success"> add</button>
+
+           </div>
+        </div>
+        <div class="col">
+<div class="input-group d-flex">
+    <input  type="number" class="form-control" placeholder="number de jour">
+    <button class="btn btn-info">reinstialiser</button>
+</div>
+        </div>
+    </div>
+</div>
